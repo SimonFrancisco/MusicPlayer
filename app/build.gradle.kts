11475ac5a4284ceec43b3fb4fac4b340e78a9e39
Simon Francisco <121758120+SimonFrancisco@users.noreply.github.com>
2025-03-28 18:47:39 +0700

    Add needed dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,16 +2,19 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
     namespace = "francisco.simon.musicplayer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "francisco.simon.musicplayer"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -43,17 +46,28 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.bundles.compose.debug)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+
+    ksp(libs.bundles.ksp)
+
+    implementation(libs.dagger2)
+
+    implementation(libs.room.core)
+
+    implementation(libs.bundles.coil)
+
+    implementation(libs.bundles.http)
+
+    implementation(libs.bundles.retrofit)
+
+    implementation(libs.json.serialization)
+
 }
--- app/src/main/java/francisco/simon/musicplayer/MainActivity.kt
+++ app/src/main/java/francisco/simon/musicplayer/MainActivity.kt
@@ -38,10 +38,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MusicPlayerTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MusicPlayerTheme {
+//        Greeting("Android")
+//    }
+//}
